d7c8701a14a0d1fd0a41228c52d9e371bbbbf68a
Heejin Yoon <heejin.yoon@wisc.edu>
2023-03-19 22:56:58 -0500

    updates

--- Second Half/Problem Sets/ps3/ps3_compute.jl
+++ Second Half/Problem Sets/ps3/ps3_compute.jl
@@ -14,11 +14,26 @@ prim, res = Initialize()
 
 ## Q2. 
 
-λ_1step, minval_1step = solve_GMM("Newton", "1step")
-# (0.6194078867223469, 234.76077311621532)
+λ_1step, minval_1step = solve_GMM("Newton", "1step") # (0.6194078867223469, 234.76077311621532)
 
 
 ## Q3. 
 
-λ_2step, minval_2step = solve_GMM("Newton", "2step")
-# (0.6920209245379816, 163.13409857517448)
+λ_2step, minval_2step = solve_GMM("Newton", "2step") # (0.6920209245379816, 163.13409857517448)
+
+
+##
+
+λ_grid = collect(0.0:0.05:1.0)
+objftn_1step = zeros(length(λ_grid))
+objftn_2step = zeros(length(λ_grid))
+
+for λ_index = 1:length(λ_grid)
+    println(λ_index)
+    objftn_1step[λ_index] = object_function(λ_grid[λ_index], "Newton", "1step")
+    objftn_2step[λ_index] = object_function(λ_grid[λ_index], "Newton", "2step")
+end
+
+plot(λ_grid, [objftn_1step, objftn_2step], label = ["1-Step GMM" "2-Step GMM"], xlabel = "λ", ylabel = "Objective Function", lw=1.5)
+plot!([λ_1step; λ_2step], [minval_1step; minval_2step], seriestype=:scatter, label="Optimum", mc=:gray, ms=3, ma=0.8)
+savefig(rt * "/Second Half/Problem Sets/ps3/gmm_objectftn.png")
